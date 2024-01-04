766b7c932e8f3b7401ee66730617b4570ba10346
IsraelIyke <nwangwuisrael@gmail.com>
2024-01-04 15:57:39 +0100

    ok

--- app/Components/nav.js
+++ app/Components/nav.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className=" fixed h-[5rem] w-screen border-b-2 border-[1px] flex justify-between items-center font-[600] backdrop-blur-sm z-10 min-w-fit">
+    <nav className=" fixed h-[5rem] w-screen border-b-2 border-[1px] flex justify-between items-center font-[600] backdrop-blur-sm z-10  max-w-[1000px]">
       <Link href="/">
         <h3 className=" pl-[1rem]">
           ug<span className="text-[#FF5C00] -ml-1">Project</span>
