564c8cbbe67c65e7b4c5279e4049564d33e330b2
IsraelIyke <nwangwuisrael@gmail.com>
2024-01-04 08:55:28 +0100

    ok

--- app/resume/template/page.js
+++ app/resume/template/page.js
@@ -1,10 +1,14 @@
 import DownloadImageComponent from "@/app/Components/CanvasComponents";
 import Footer from "@/app/Components/footer";
 import Nav from "@/app/Components/nav";
+import Head from "next/head";
 
 export default function Template() {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="width=1992px, initial-scale=1.0" />
+      </Head>
       <Nav />
       <br />
       <br />
